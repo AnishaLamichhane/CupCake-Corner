443ed6327e54d5fa07cddd47db939c5da99d94b9
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-05-31 09:50:04 +0545

    Final touch

--- project 10/CheckoutView.swift
+++ project 10/CheckoutView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var conformationMessage = ""
+    @State private var showingConformation = false
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -23,7 +26,7 @@ struct CheckoutView: View {
                         .padding()
                     
                     Button ("Place Order") {
-//                        place the order
+                        placeOrder()
                     }
                     .padding()
                 }
@@ -31,6 +34,37 @@ struct CheckoutView: View {
             
         }
         .navigationBarTitle("Checkout", displayMode: .inline)
+        .alert(isPresented: $showingConformation){
+            Alert(title: Text("Thank You"), message: Text(conformationMessage), dismissButton: .default(Text("Okay")))
+        }
+    }
+    
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("failed to encode items")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "Post"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request){ data, resopnse, error in
+            guard let data = data else {
+                print("No data in response \(error?.localizedDescription ?? "Unknown Error").")
+                return
+            }
+            
+            if let decodedOrder  = try? JSONDecoder().decode(Order.self, from: data) {
+                self.conformationMessage = "Your order for \(decodedOrder.quantity) * \(Order.types[decodedOrder.type].lowercased()) cupcakes in on the way."
+                self.showingConformation = true
+            } else {
+                print("Invalid response from the server.")
+            }
+        }.resume()
+        
     }
 }
 
